f735c95f5d802b9fa7409dfa571f0dd000eacea9
Sajed Zarinpour Nashroudkoli <sa.zarinpour@gmail.com>
2025-12-28 01:49:39 -0800

    adding policy for task access and registering general exceptions for them to use in middlewares while keep info compartmenalised

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +29,21 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * To handle general errors
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json(['error' => 'Task not found.'], 404);
+        } 
+        
+        if ($exception instanceof AuthorizationException) {
+            return response()->json(['error' => $exception->getMessage()], 403);
+        }
+
+        return parent::render($request, $exception);
     }
 }
