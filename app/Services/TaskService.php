f5ba72a7f4ab9a8907a0a8b5034ee9d04a50b9d2
Sajed Zarinpour Nashroudkoli <sa.zarinpour@gmail.com>
2025-12-28 00:01:51 -0800

    adding task service

---
+++ app/Services/TaskService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
+class TaskService {
+    public function Create(array $data){
+        return Task::create($data);
+    }
+    public function Update(int $id, array $data){
+        return Task::find($id)->update($data);
+    }
+    public function Delete(int $id){
+        return Task::find($id)->delete();
+    }
+    public function Get(int $id){
+        return Task::find($id);
+    }
+    public function My(User $user){
+        return $user->tasks();
+    }
+}
