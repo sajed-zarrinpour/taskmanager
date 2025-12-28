73115b58c9abab68eff8dd3974745172e15b2d0a
Sajed Zarinpour Nashroudkoli <sa.zarinpour@gmail.com>
2025-12-28 01:50:52 -0800

    modify task resource to limit data presentation

--- app/Http/Resources/TaskResource.php
+++ app/Http/Resources/TaskResource.php
@@ -14,6 +14,12 @@ class TaskResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "id"=> $this->id,
+            "title" => $this->title,
+            "status" => $this->status,
+            "created_at"=>$this->created_at,
+            "updated_at"=> $this->updated_at,
+        ];
     }
 }
