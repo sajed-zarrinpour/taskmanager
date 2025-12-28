c9be84502deb2f08370ddab8b27c33f53d6129b6
Sajed Zarinpour Nashroudkoli <sa.zarinpour@gmail.com>
2025-12-28 01:52:17 -0800

    refactoring task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,21 +4,66 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TaskDataRequest;
 use App\Http\Resources\TaskResource;
+use App\Models\Task;
 use App\Services\AuthenticationService;
 use App\Services\TaskService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Gate;
 class TaskController extends Controller
 {
     public function __construct(private TaskService $taskService, private AuthenticationService $authService) {}
 
+    /**
+     * handles creation of a task. any authenticated user can create a task.
+     */
     public function create(TaskDataRequest $request) {
         $validated = $request->validated();
-        $data = [
+
+        $task = $this->taskService->create([
             ...$validated,
             'user_id'=>auth()->user()->id,
-        ];
-        $task = $this->taskService->create($data);
+        ]);
+
         return response()->json(new TaskResource($task), 201);
+    }
+
+    /**
+     * ;ist of all the tasks of the current user
+     */
+    public function my(Request $request) {
+
+        $perPage = $request->input('per_page', 1);
+        
+        $tasks = $this->taskService->MyTasks(auth()->user(), $perPage);
+
+        return TaskResource::collection($tasks)->additional([
+            'meta' => [
+                'current_page' => $tasks->currentPage(),
+                'last_page' => $tasks->lastPage(),
+                'per_page' => $tasks->perPage(),
+                'total' => $tasks->total(),
+                'links' => [
+                    'next' => $tasks->nextPageUrl(),
+                    'prev' => $tasks->previousPageUrl(),
+                ]
+            ],
+        ]);
+    }
+
+    public function detail(Request $request, Task $task){
+        return new TaskResource($task);
+    }
+
+    public function update(TaskDataRequest $request, Task $task) {
+        $validated = $request->validated();
+        $task = $this->taskService->update($task->id, $validated);
+        return new TaskResource($task);
+    }
+
+    public function delete(Task $task) {
+        $this->taskService->Delete($task);
+        return new TaskResource(new Task());
     }
 }
