1437ef32440f8536096cb5e8b38348a36cf2c85b
Sajed Zarinpour Nashroudkoli <sa.zarinpour@gmail.com>
2025-12-28 21:25:35 -0800

    adding task factory for testing

--- app/Enums/TaskStatus.php
+++ app/Enums/TaskStatus.php
@@ -5,4 +5,10 @@ enum TaskStatus : string {
     case PENDING = 'pending';
     case INPROGRESS = 'in_progress';
     case DONE='done';
+
+    public static function getValues(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
 }
